bee85faa7fbc2f0558bb52dc23e89e2eb4f99931
Salim Almamari <salimalmamari962@gmail.com>
2026-09-24 02:44:02 +0000

    sadasd

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Almamari_NewMVC.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
